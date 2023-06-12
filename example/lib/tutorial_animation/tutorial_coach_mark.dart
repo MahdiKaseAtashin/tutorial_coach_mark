41403100d065e643d59be10c4ac417e4a133bd97
Mahdi Kase Atashin <mdn.dev@gmail.com>
2023-06-12 14:27:11 +0330

    feat: optimize tutorial coach mark

---
+++ example/lib/tutorial_animation/tutorial_coach_mark.dart
@@ -0,0 +1,121 @@
+library tutorial_coach_mark;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'target/target_focus.dart';
+import 'widgets/tutorial_coach_mark_widget.dart';
+
+export 'target/target_content.dart';
+export 'target/target_focus.dart';
+export 'target/target_position.dart';
+export 'util.dart';
+
+class TutorialCoachMark {
+  final List<TargetFocus> targets;
+  final FutureOr<void> Function(TargetFocus)? onClickTarget;
+  final FutureOr<void> Function(TargetFocus, TapDownDetails)?
+      onClickTargetWithTapPosition;
+  final FutureOr<void> Function(TargetFocus)? onClickOverlay;
+  final Function()? onFinish;
+  final double paddingFocus;
+  final Function()? onSkip;
+  final AlignmentGeometry alignSkip;
+  final String textSkip;
+  final TextStyle textStyleSkip;
+  final bool hideSkip;
+  final Color colorShadow;
+  final double opacityShadow;
+  final GlobalKey<TutorialCoachMarkWidgetState> _widgetKey = GlobalKey();
+  final Duration focusAnimationDuration;
+  final Duration unFocusAnimationDuration;
+  final Duration pulseAnimationDuration;
+  final bool pulseEnable;
+  final Widget? skipWidget;
+  final bool showSkipInLastTarget;
+
+  OverlayEntry? _overlayEntry;
+
+  TutorialCoachMark({
+    required this.targets,
+    this.colorShadow = Colors.black,
+    this.onClickTarget,
+    this.onClickTargetWithTapPosition,
+    this.onClickOverlay,
+    this.onFinish,
+    this.paddingFocus = 10,
+    this.onSkip,
+    this.alignSkip = Alignment.bottomRight,
+    this.textSkip = "SKIP",
+    this.textStyleSkip = const TextStyle(color: Colors.white),
+    this.hideSkip = false,
+    this.opacityShadow = 0.8,
+    this.focusAnimationDuration = const Duration(milliseconds: 600),
+    this.unFocusAnimationDuration = const Duration(milliseconds: 600),
+    this.pulseAnimationDuration = const Duration(milliseconds: 500),
+    this.pulseEnable = true,
+    this.skipWidget,
+    this.showSkipInLastTarget = true,
+  }) : assert(opacityShadow >= 0 && opacityShadow <= 1);
+
+  OverlayEntry _buildOverlay({bool rootOverlay = false}) {
+    return OverlayEntry(
+      builder: (context) {
+        return TutorialCoachMarkWidget(
+          key: _widgetKey,
+          targets: targets,
+          clickTarget: onClickTarget,
+          onClickTargetWithTapPosition: onClickTargetWithTapPosition,
+          clickOverlay: onClickOverlay,
+          paddingFocus: paddingFocus,
+          onClickSkip: skip,
+          alignSkip: alignSkip,
+          skipWidget: skipWidget,
+          textSkip: textSkip,
+          textStyleSkip: textStyleSkip,
+          hideSkip: hideSkip,
+          colorShadow: colorShadow,
+          opacityShadow: opacityShadow,
+          focusAnimationDuration: focusAnimationDuration,
+          unFocusAnimationDuration: unFocusAnimationDuration,
+          pulseAnimationDuration: pulseAnimationDuration,
+          pulseEnable: pulseEnable,
+          finish: finish,
+          rootOverlay: rootOverlay,
+          showSkipInLastTarget: showSkipInLastTarget,
+        );
+      },
+    );
+  }
+
+  void show({required BuildContext context, bool rootOverlay = false}) {
+    Future.delayed(Duration.zero, () {
+      if (_overlayEntry == null) {
+        _overlayEntry = _buildOverlay(rootOverlay: rootOverlay);
+        // ignore: invalid_null_aware_operator
+        Overlay.of(context, rootOverlay: rootOverlay)?.insert(_overlayEntry!);
+      }
+    });
+  }
+
+  void finish() {
+    onFinish?.call();
+    _removeOverlay();
+  }
+
+  void skip() {
+    onSkip?.call();
+    _removeOverlay();
+  }
+
+  bool get isShowing => _overlayEntry != null;
+
+  void next() => _widgetKey.currentState?.next();
+
+  void previous() => _widgetKey.currentState?.previous();
+
+  void _removeOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+}
